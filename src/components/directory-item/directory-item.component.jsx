c326cf7149ea70433f6245a6d8130fc5eb598593
Anish Kumar Dubey <kumaranishdubey@gmail.com>
2022-11-15 16:55:49 +0530

    fixing clashing styles

---
+++ src/components/directory-item/directory-item.component.jsx
@@ -0,0 +1,16 @@
+import './directory-item.styles.scss';
+const DirectoryItem = ({ category: { imageUrl, title } }) => {
+	return (
+		<div className='directory-item-container'>
+			<div
+				className='background-image'
+				style={{ backgroundImage: `url(${imageUrl})` }}
+			/>
+			<div className='body'>
+				<h2>{title}</h2>
+				<p>Show Now</p>
+			</div>
+		</div>
+	);
+};
+export default DirectoryItem;
